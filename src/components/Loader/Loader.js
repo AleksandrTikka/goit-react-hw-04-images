9b10bf281bd38d99ba0b72561ce9186540d7fe10
AleksandrTikka <alexandertikka@gmail.com>
2022-09-08 23:47:43 +0300

    Loader style

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,17 +1,20 @@
 import { Watch } from 'react-loader-spinner';
+import css from './Loader.module.css';
 
 const Loader = () => {
   return (
-    <Watch
-      height="80"
-      width="80"
-      radius="48"
-      color="#4fa94d"
-      ariaLabel="watch-loading"
-      wrapperStyle={{}}
-      wrapperClassName=""
-      visible={true}
-    />
+    <div className={css.loader}>
+      <Watch
+        height="80"
+        width="80"
+        radius="48"
+        color="#4fa94d"
+        ariaLabel="watch-loading"
+        wrapperStyle={{}}
+        wrapperClassName=""
+        visible={true}
+      />
+    </div>
   );
 };
 
