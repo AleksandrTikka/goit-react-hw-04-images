86c2cc234a4b41728332baaa130ee26c55c8cf60
AleksandrTikka <alexandertikka@gmail.com>
2022-09-17 00:54:40 +0300

    changed id to index

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -4,13 +4,16 @@ import PropTypes from 'prop-types';
 const ImageGallery = ({ images }) => {
   return (
     <ul className={css.imageGallery}>
-      {images.map(image => {
-        return <ImageGalleryItem image={image} key={image.id} />;
+      {images.map((image, index) => {
+        return <ImageGalleryItem image={image} key={index} />;
       })}
     </ul>
   );
 };
 ImageGallery.propTypes = {
-  images: PropTypes.array.isRequired,
+  images: PropTypes.array,
+  image: PropTypes.shape({
+    key: PropTypes.number.isRequired,
+  }),
 };
 export default ImageGallery;
